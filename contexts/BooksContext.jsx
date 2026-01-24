1cedca5b51ae506f650c32fcdfc5370c6f84114f
Vimal2023 <vimal83500@gmail.com>
2026-01-25 03:24:17 +0530

    Fetching the book records

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,30 +1,36 @@
-import { createContext, useState } from "react"
-import { databases } from "../lib/appwrite"
-import { ID, Permission, Role } from "react-native-appwrite"
-import { useUser } from "../hooks/useUser"
+import { createContext, useEffect, useState } from "react";
+import { databases } from "../lib/appwrite";
+import { ID, Permission, Query, Role } from "react-native-appwrite";
+import { useUser } from "../hooks/useUser";
 
-const DATABASE_ID = "6973ea650002b612230f"
-const COLLECTION_ID = "books"
+const DATABASE_ID = "6973ea650002b612230f";
+const COLLECTION_ID = "books";
 
-export const BooksContext = createContext()
+export const BooksContext = createContext();
 
-export function BooksProvider({children}) {
-  const [books, setBooks] = useState([])
-  const { user } = useUser()
+export function BooksProvider({ children }) {
+  const [books, setBooks] = useState([]);
+  const { user } = useUser();
 
   async function fetchBooks() {
     try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID,
+        [Query.equal("userId", user.$id)],
+      );
 
+      setBooks(response.documents);
+      console.log(response.documents);
     } catch (error) {
-      console.error(error.message)
+      console.error(error.message);
     }
   }
 
   async function fetchBookById(id) {
     try {
-
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
   }
 
@@ -34,31 +40,40 @@ export function BooksProvider({children}) {
         DATABASE_ID,
         COLLECTION_ID,
         ID.unique(),
-        {...data, userId: user.$id},
+        { ...data, userId: user.$id },
         [
           Permission.read(Role.user(user.$id)),
           Permission.update(Role.user(user.$id)),
           Permission.delete(Role.user(user.$id)),
-        ]
-      )
+        ],
+      );
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
   }
 
   async function deleteBook(id) {
     try {
-      
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
   }
 
+  useEffect(() => {
+
+    if (user) {
+      fetchBooks()
+    } else {
+      setBooks([])
+    }
+
+  }, [user])
+
   return (
-    <BooksContext.Provider 
+    <BooksContext.Provider
       value={{ books, fetchBooks, fetchBookById, createBook, deleteBook }}
     >
       {children}
     </BooksContext.Provider>
-  )
+  );
 }
